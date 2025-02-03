f86d60176b02b7e104dd4e092986e57413c41bc3
Mugdho <saha2171515@stud.kuet.ac.bd>
2025-02-03 13:01:11 +0600

    03/02/2025

--- src/Pages/login.jsx
+++ src/Pages/login.jsx
@@ -55,8 +55,9 @@ function Login() {
                         <div className="col-lg-7 col-md-6 col-sm-5 d-none d-lg-block">
                             <div className="banner-content">
                                 <h1 style={{fontSize: '88px'}}>Registration {/*<span style={{textTransform:"lowercase"}}>for</span>*/}</h1>
-                                <h3 style={{marginBottom:'0', marginTop:'20px'}}>Toilet Conference 2025</h3>
-                                <p style={{marginBottom:'0', marginTop:'20px'}}>Dhaka, Bangladesh | 25-26 February 2025</p>
+                                <h3 style={{marginBottom: '0', marginTop: '20px'}}>Toilet Conference 2025</h3>
+                                <p style={{marginBottom: '0', marginTop: '20px'}}>Dhaka, Bangladesh | 25-26 February
+                                    2025</p>
                             </div>
                         </div>
                         <div className="col-lg-5">
@@ -167,13 +168,144 @@ function Login() {
                         <div className="col-xl-3 col-lg-3 col-md-3 col-sm-4 col-4 pt-3">
                             <div className="xs-mb30">
                                 <div className="spk-img">
-                                    <img className="img-fluid" src="assets/img/convenors/4.png" style={{padding:'20px'}} alt="trainer-img"/>
+                                    <img className="img-fluid" src="assets/img/convenors/4.png"
+                                         style={{padding: '20px'}} alt="trainer-img"/>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <div className="whos-speaking-area pad100">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="section-title text-center">
+                                <div className="title-text mb50 xs-mb40">
+                                    <h2>Gold Partners</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row d-flex align-items-center justify-content-center">
+                        <div className="col-xl-2 col-lg-2 col-md-3 col-sm-4 col-4 pt-3">
+                            <div className="xs-mb30">
+                                <div className="spk-img">
+                                    <img className="img-fluid"
+                                         src="https://cdn.prod.website-files.com/66e902391a56b034490ebf7e/678c7d472a97d5cedc37cb9a_2_%20BRAC%20logo.png"
+                                         alt="trainer-img"/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-xl-2 col-lg-2 col-md-3 col-sm-4 col-4 pt-3">
+                            <div className="xs-mb30">
+                                <div className="spk-img">
+                                    <img className="img-fluid"
+                                         src="https://cdn.prod.website-files.com/66e902391a56b034490ebf7e/678cd703337ee533c7c2e978_Black%20White%20Modern%20Handwritten%20Square%20Studio%20Logo%20(1).png"
+                                         alt="trainer-img"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="whos-speaking-area pad100">
+                <div className="container">
+                    <div className="row d-flex align-items-center justify-content-center">
+                        <div className="col-3 col-md-3 col-sm-12">
+                            <div className="row">
+                                <div className="col-lg-12">
+                                    <div className="section-title text-center">
+                                        <div className="title-text mb50 xs-mb40">
+                                            <h2 style={{fontSize: "24px"}}>Tech Partner</h2>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="xs-mb30">
+                                    <div className="spk-img">
+                                        <img className="img-fluid"
+                                             src="https://cdn.prod.website-files.com/66e902391a56b034490ebf7e/678f6f362931d515dd4d8813_FrogBID%20Logo-p-500.png"
+                                             alt="trainer-img"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-1">
+
+                        </div>
+                        <div className="col-3 col-md-3 col-sm-12">
+                            <div className="row">
+                                <div className="col-lg-12">
+                                    <div className="section-title text-center">
+                                        <div className="title-text mb50 xs-mb40">
+                                            <h2 style={{fontSize: "24px"}}>Hospitality partner</h2>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="xs-mb30">
+                                    <div className="spk-img">
+                                        <img className="img-fluid"
+                                             src="https://cdn.prod.website-files.com/66e902391a56b034490ebf7e/678debd790f45434f015e1ac_intercontinental_logo_BX7-p-500.png"
+                                             alt="trainer-img"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-1">
+
+                        </div>
+                        <div className="col-3 col-md-3 col-sm-12">
+                            <div className="row">
+                                <div className="col-lg-12">
+                                    <div className="section-title text-center">
+                                        <div className="title-text mb50 xs-mb40">
+                                            <h2 style={{fontSize: "24px"}}>Payment gateway partner</h2>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="xs-mb30">
+                                    <div className="spk-img">
+                                        <img className="img-fluid"
+                                             src="https://cdn.prod.website-files.com/66e902391a56b034490ebf7e/6799d721742ca1725b5e493f_LOGO_SSLCOMMERZ-p-500.png"
+                                             alt="trainer-img"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="whos-speaking-area pad100">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="section-title text-center">
+                                <div className="title-text mb50 xs-mb40">
+                                    <h2>Contributing partner</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row d-flex align-items-center justify-content-center">
+                        <div className="col-xl-2 col-lg-2 col-md-3 col-sm-4 col-4 pt-3">
+                            <div className="xs-mb30">
+                                <div className="spk-img">
+                                    <img className="img-fluid"
+                                         src="https://cdn.prod.website-files.com/66e902391a56b034490ebf7e/6799dcbdcff0fcdf98bcfdb1_bkash-logo-png_seeklogo-273684.png"
+                                         alt="trainer-img"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <div className="whos-speaking-area pad100">
                 <div className="container">
                     <div className="row">
